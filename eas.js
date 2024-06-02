176f9038f2f3b5ba4f5870ea75d660412f20ad1e
Magnimius <ziyaadshaboodien@gmail.com>
2024-06-02 14:26:12 +0200

    added new functionality to include a selection menu for how many blocks appear. can't seem to get it to be even squares in the grid.

--- eas.js
+++ eas.js
@@ -1,28 +1,45 @@
 
 const cont = document.getElementById("cont");
-
 function createDivs(numDivs) {
+
     for (let i = 0; i < numDivs; i++) {
         var panel = document.createElement("div");
         panel.className = "track10";
         panel.id="track10";
-        panel.classList.add("style.css");
+        width = 400/Math.sqrt(numDivs);
+        flex = 100/Math.sqrt(numDivs);
+        flexString = flex.toString();
+        widthString = width.toString();
+        panel.setAttribute("style","height:"+widthString+"px;width:"+widthString+"px;")
         cont.appendChild(panel);
     }
-}
-
-createDivs(132);
-
-var divvy = document.querySelectorAll("div");
-for (const divver of divvy) {
+    var divvy = document.querySelectorAll("div");
+    for (const divver of divvy) {
     divver.addEventListener("mouseover",function(e) {
         divver.style.backgroundColor="black";
     })
 }
+}
+
+function removeAllChildren() {
+    while(cont.firstChild) {
+        cont.removeChild(cont.firstChild);
+    }
+}
+
+const selector = document.getElementById("selector");
+var divNum = selector.value;
+
+selector.addEventListener("change",function(e){
+    removeAllChildren();
+    divNum = selector.value;
+    createDivs(divNum);
+})
 
 const reset = document.getElementById("reset");
 
 reset.addEventListener("click",function(e) {
+    var divvy = document.querySelectorAll("div");
     for (const divver of divvy) {
         divver.style.backgroundColor="white";
     }
